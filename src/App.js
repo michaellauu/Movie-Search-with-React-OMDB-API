807581b79facf15a1d7e6d9f5e1ca9c3aa131b2a
mlau10 <mlau10@ucsc.edu>
2022-06-17 04:43:51 -0700

    Introduced predone css and search icon svg. App currently console logs movies.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
+import './App.css';
+import  SearchIcon from './search.svg';
 
+const API_URL = " http://www.omdbapi.com?apikey=5dd576a6";
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Shrek');
+    }, []);
+
+
     return (
         <h1>App</h1>
     );
