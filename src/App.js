9a9b8eaada7e3a442345c6a193ad36d89a4ac947
mlau10 <mlau10@ucsc.edu>
2022-06-17 04:52:24 -0700

    app now displays a static movie of my choice

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import  SearchIcon from './search.svg';
 
 const API_URL = " http://www.omdbapi.com?apikey=5dd576a6";
 
+const movie1 = {
+        "Title": "Shrek 4-D",
+        "Year": "2003",
+        "imdbID": "tt0360985",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNDVlOWZkNTEtNTcxZS00NDVhLWFlZWItYWFhNmZmZWNhYzU1XkEyXkFqcGdeQXVyNzMwOTY2NTI@._V1_SX300.jpg"
+}
+
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -19,7 +27,34 @@ const App = () => {
 
 
     return (
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input 
+                    placeholder="Search for movie titles."
+                    value="Superman"
+                    onChange={() => {}}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
     );
 }
 
